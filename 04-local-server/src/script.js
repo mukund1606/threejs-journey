997f24f6dd3a9e25e996db153198676d83e142e5
Mukund Mittal <mukund.mukulmittal@gmail.com>
2023-08-01 18:58:09 +0530

    Tutorial 4 and 5 Completed

---
+++ 04-local-server/src/script.js
@@ -0,0 +1,34 @@
+import * as THREE from "three";
+
+// Scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.z = 3;
+scene.add(camera);
+
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width / 2, sizes.height / 2);
+
+renderer.render(scene, camera);
