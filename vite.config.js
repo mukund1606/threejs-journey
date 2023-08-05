ac08dd09261c45c23cf3fe6bab4496d10999294e
Mukund Mittal <mukund.mukulmittal@gmail.com>
2023-08-06 02:30:59 +0530

    Live Page Routes Set for Production

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,38 @@
-const isCodeSandbox =
-  "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
-
-export default {
+import { resolve } from "path";
+import { defineConfig } from "vite";
+export default defineConfig({
   root: "src/",
   publicDir: "../static/",
   base: "./",
-  server: {
-    host: true,
-    open: !isCodeSandbox, // Open if it's not a CodeSandbox
-  },
   build: {
     outDir: "../dist",
-    emptyOutDir: true,
     sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        "03-basic-scene": resolve(__dirname, "src/03-basic-scene/index.html"),
+        "04-local-server": resolve(
+          __dirname,
+          "src/04-local-server/src/index.html"
+        ),
+        "05-transform-objects": resolve(
+          __dirname,
+          "src/05-transform-objects/src/index.html"
+        ),
+        "06-animations": resolve(__dirname, "src/06-animations/src/index.html"),
+        "07-cameras": resolve(__dirname, "src/07-cameras/src/index.html"),
+        "08-fullscreen-and-resizing": resolve(
+          __dirname,
+          "src/08-fullscreen-and-resizing/src/index.html"
+        ),
+        "09-geometries": resolve(__dirname, "src/09-geometries/src/index.html"),
+        "10-debug-ui": resolve(__dirname, "src/10-debug-ui/src/index.html"),
+        "11-textures": resolve(__dirname, "src/11-textures/src/index.html"),
+        "12-materials": resolve(__dirname, "src/12-materials/src/index.html"),
+        "13-3d-text": resolve(__dirname, "src/13-3d-text/src/index.html"),
+        "14-go-live": resolve(__dirname, "src/14-go-live/dist/index.html"),
+      },
+    },
   },
-};
+});
